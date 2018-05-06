3de9b1240529fd009a6d32386c576d0fcc508e11
felipe hernandez <Felipe_Sue@felipesMini1764.hsd1.ca.comcast.net>
2018-05-06 00:26:21 -0700

    I can add to do items yay

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
+//
+// const myForms = document.forms;
+// console.log(myForms[0]);
 
-const myForms = document.forms;
-console.log(myForms[0]);
+// const itmDone = document.querySelector('li');
+// function markDone(){
+// itmDone.addEventListener('click',(e)=>{
+//     itmDone.classList.add('done');
+// })
+//
+// }
+// markDone();
 
-const itmDone = document.querySelector('li');
-function markDone(){
-itmDone.addEventListener('click',(e)=>{
-    itmDone.classList.add('done');
-})
+
+document.getElementById('button').addEventListener('click' , function(){
+  let itmEntered = document.getElementById('text-input').value;
+  if (itmEntered) {
+    addToList(itmEntered);
+    document.getElementById('text-input').value = "";
+  }else {alert('Looks like you forgot to add something to do!');
+}
+
+});
+
+function addToList(text){
+    let listItems = document.getElementById('listItems');
+
+    let newItem = document.createElement('li');
+    newItem.innerText = text;
+
+    listItems.insertBefore(newItem, listItems.childNodes[0]);
 
 }
-markDone();
