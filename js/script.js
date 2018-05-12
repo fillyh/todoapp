8c07307f363717b0e4e8574dbceeaaeeb9f5391b
felipe hernandez <Felipe_Sue@felipesMini1764.hsd1.ca.comcast.net>
2018-05-12 10:07:39 -0700

    I can hide completed items

--- js/script.js
+++ js/script.js
@@ -1,37 +1,4 @@
-// 
-//  document.querySelector("#text-input").addEventListener('keypress', function (e){
-//   if (e.which === 13){
-//   }
-// });
-// console.log(hitEnter);
-
-
-
-function markDone(){
-let todoList = document.querySelector('UL');
-todoList.addEventListener('click', function(e) {
-  if (e.target.tagName === 'LI') {
-    e.target.classList.toggle('done');
-  }
-}, false);
-console.log(todoList);
-}
-markDone();
-
-function clickButton(){
-document.getElementById('button').addEventListener('click' , function(){
-  let itmEntered = document.getElementById('text-input').value;
-  if (itmEntered) {
-    addToList(itmEntered);
-    document.getElementById('text-input').value = "";
-  }else {alert('Looks like you forgot to add something to do!');
-}
-});
-}
-
-clickButton()
-
-
+//Elements that need to be created when the button is clicked
 function addToList(text){
     let todo = document.getElementById('todo');
 
@@ -42,5 +9,54 @@ function addToList(text){
     todo.insertBefore(newItem, todo.childNodes[0]);
 
     document.getElementById('placeHolder').style.display = "none";
-
 }
+
+//Clicking the button adds Elements
+function clickButton(){
+document.getElementById('button').addEventListener('click' , function(){
+  let itmEntered = document.getElementById('text-input').value;
+  if (itmEntered) {
+    addToList(itmEntered);
+    document.getElementById('text-input').value = "";
+  }else {alert('Looks like you forgot to add something to do!');
+}
+});
+}
+clickButton();
+
+//Marks completed elements done
+function markDone(){
+let todoList = document.querySelector('UL');
+todoList.addEventListener('click', function(e) {
+  if (e.target.tagName === 'LI') {
+    // e.target.contentEditable = true;
+    e.target.classList.toggle('done');
+    // todoList.removeChild(todoList.childNodes[0]); //removes item
+  }
+}, false);
+console.log(todoList);
+console.log(todoList.childNodes[0]);
+}
+markDone();
+
+
+  // document.querySelector("#text-input").addEventListener('keypress', function(e) {
+  //   e.preventDefault();
+  //   if (e.keyCode ===13){
+  //     let itmEntered = document.getElementById('text-input').value;
+  //     addToList(itmEntered);
+  //     document.getElementById('text-input').value="";
+  //     document.getElementById("button").click();
+  //   }
+  // })
+
+
+  function viewToDo(){
+  document.getElementById('viewToDo').addEventListener('click' , function(e){
+    let hideComplete = document.getElementsByClassName('done');
+    for (var i = 0; i < hideComplete.length; i++){
+      hideComplete[i].style.display = "none";
+    }
+  });
+  }
+  viewToDo();
