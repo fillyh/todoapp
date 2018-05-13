4efb170df9d86319916c0636a13cb28e323f8c4b
felipe hernandez <felipehernandez@felipes-MacBook-Pro.local>
2018-05-12 18:06:27 -0700

    tabs working now

--- js/script.js
+++ js/script.js
@@ -30,12 +30,13 @@ let todoList = document.querySelector('UL');
 todoList.addEventListener('click', function(e) {
   if (e.target.tagName === 'LI') {
     // e.target.contentEditable = true;
-    e.target.classList.toggle('done');
+    // e.target.classList.remove('todoItm');
+    // e.target.classList.toggle('done');
+    e.target.classList.replace('todoItm', 'done');
     // todoList.removeChild(todoList.childNodes[0]); //removes item
   }
 }, false);
 console.log(todoList);
-console.log(todoList.childNodes[0]);
 }
 markDone();
 
@@ -50,23 +51,68 @@ markDone();
   //   }
   // })
 
-
+//Tab Menu section
+let todoItm = document.getElementsByClassName("todoItm");
+let complete = document.getElementsByClassName('done');
+let ulList = document.getElementsByTagName('LI');
+//view Todo Items
   function viewToDo(){
   document.getElementById('viewToDo').addEventListener('click' , function(e){
-    let hideComplete = document.getElementsByClassName('done');
-    for (var i = 0; i < hideComplete.length; i++){
-      hideComplete[i].style.display = "none";
+    for (var i = 0; i < complete.length; i++){
+      complete[i].style.display = "none";
     }
   });
   }
   viewToDo();
 
+  function displayTodo(){
+      document.getElementById('viewToDo').addEventListener('click' , function(e){
+    for (var i = 0; i < todoItm.length; i++){
+      todoItm[i].style.display = "block";
+    }
+  });
+}
+displayTodo();
+
+
+//View All Items
   function viewAll(){
   document.getElementById('viewAll').addEventListener('click' , function(e){
-    let showComplete = document.getElementsByClassName('done');
-    for (var i = 0; i < showComplete.length; i++){
-      showComplete[i].style.display = "block";
+    for (var i = 0; i < ulList.length; i++){
+      ulList[i].style.display = "block";
     }
   });
   }
   viewAll();
+
+//View Comleted Items
+  function viewCompleted(){
+  document.getElementById('viewDone').addEventListener('click' , function(e){
+    for (var i = 0; i < todoItm.length; i++){
+      todoItm[i].style.display = "none";
+    }
+  });
+  }
+  viewCompleted();
+
+  function displayCompleted(){
+      document.getElementById('viewDone').addEventListener('click' , function(e){
+        for (var i = 0; i < complete.length; i++){
+          complete[i].style.display = "block";
+        }
+  });
+}
+displayCompleted();
+
+// function viewCompleted(){
+// document.getElementById('viewDone').addEventListener('click' , function(e){
+//   for (var i = 0; i < todoItm.length; i++){
+//     todoItm[i].style.display = "none";
+//     let showCompleted = todoItm[i].classList.contains('done');
+//     showCompleted.style.display = "block";
+//   }
+//
+// });
+//
+// }
+// viewCompleted();
