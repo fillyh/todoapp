c104f04257521ab96f84854630644ed5bf21e3bb
felipe hernandez <Felipe_Sue@felipesMini1764.hsd1.ca.comcast.net>
2018-05-13 00:55:56 -0700

    almost there

--- js/script.js
+++ js/script.js
@@ -8,6 +8,14 @@ function addToList(text){
 
     todo.insertBefore(newItem, todo.childNodes[0]);
 
+    // todoBtn.classList.remove('btns-active');
+    // allBtn.classList.add('btns-active');
+    // doneBtn.classList.remove('btns-active');
+    for (var i = 0; i < ulList.length; i++){
+      ulList[i].style.display = "block";
+    }
+
+
     document.getElementById('placeHolder').style.display = "none";
 }
 
@@ -31,7 +39,7 @@ todoList.addEventListener('click', function(e) {
   if (e.target.tagName === 'LI') {
     // e.target.contentEditable = true;
     // e.target.classList.remove('todoItm');
-    // e.target.classList.toggle('done');
+    // e.target.classLisst.toggle('done');
     e.target.classList.replace('todoItm', 'done');
     // todoList.removeChild(todoList.childNodes[0]); //removes item
   }
@@ -52,12 +60,19 @@ markDone();
   // })
 
 //Tab Menu section
+let todoBtn = document.getElementById('viewToDo');
+let doneBtn = document.getElementById('viewDone');
+let allBtn = document.getElementById('viewAll');
+
 let todoItm = document.getElementsByClassName("todoItm");
 let complete = document.getElementsByClassName('done');
 let ulList = document.getElementsByTagName('LI');
 //view Todo Items
   function viewToDo(){
   document.getElementById('viewToDo').addEventListener('click' , function(e){
+    todoBtn.classList.add('btns-active');
+    allBtn.classList.remove('btns-active');
+    doneBtn.classList.remove('btns-active');
     for (var i = 0; i < complete.length; i++){
       complete[i].style.display = "none";
     }
@@ -68,7 +83,7 @@ let ulList = document.getElementsByTagName('LI');
   function displayTodo(){
       document.getElementById('viewToDo').addEventListener('click' , function(e){
     for (var i = 0; i < todoItm.length; i++){
-      todoItm[i].style.display = "block";
+    todoItm[i].style.display = "block";
     }
   });
 }
@@ -78,6 +93,9 @@ displayTodo();
 //View All Items
   function viewAll(){
   document.getElementById('viewAll').addEventListener('click' , function(e){
+    todoBtn.classList.remove('btns-active');
+    allBtn.classList.add('btns-active');
+    doneBtn.classList.remove('btns-active');
     for (var i = 0; i < ulList.length; i++){
       ulList[i].style.display = "block";
     }
@@ -88,6 +106,9 @@ displayTodo();
 //View Comleted Items
   function viewCompleted(){
   document.getElementById('viewDone').addEventListener('click' , function(e){
+    todoBtn.classList.remove('btns-active');
+    allBtn.classList.remove('btns-active');
+    doneBtn.classList.add('btns-active');
     for (var i = 0; i < todoItm.length; i++){
       todoItm[i].style.display = "none";
     }
